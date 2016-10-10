50cd31f8805e620c69e19ae1617bf732756182b3
Antonio Pierro <antonio.pierro@gmail.com>
2016-10-10 22:25:42 +0200

    Create FCFS.cpp

---
+++ FCFS.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, avwt=0, avtat=0;
+    cout << "Enter total number of processes:";
+    cin>>n;
+
+    int bt[n], wt[n], tat[n];
+
+    cout<<"\nEnter Process Burst Time\n";
+    for(i=0;i<n;i++)
+    {
+        cout << "P["<<i+1<<"]:";
+        cin >> bt[i];
+    }
+
+    wt[0] = 0; //waiting time for first process is 0
+
+    //calculating waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+
+    cout<<"\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time";
+
+    //calculating turnaround time
+    for(i = 0 ; i<n; i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        cout<<"\nP["<<i+1<<"]"<<"\t\t"<<bt[i]<<"\t\t"<<wt[i]<<"\t\t"<<tat[i];
+    }
+
+    avwt /= i;
+    avtat /= i;
+    cout << "\n\nAverage Waiting Time:"<< avwt;
+    cout << "\nAverage Turnaround Time:"<< avtat <<"\n";
+
+    return 0;
+}
