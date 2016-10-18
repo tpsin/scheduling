3fe943fe0f7dade763f8134fb3d7d73bdcc9d3a7
Antonio Pierro <antonio.pierro@gmail.com>
2016-10-18 14:20:42 +0200

    Completed the FCFS Scheduling algorithm

--- FCFS.java
+++ FCFS.java
@@ -1,3 +1,11 @@
+/**
+    Title: First-Come First-Served (FCFS) Scheduling algorithm
+    File-name: FCFS.java
+    Purpose: Calculates the Average Waiting Time and the Average Turnaround Time
+    considering the arrival time.
+    @author Antonio Pierro
+*/
+
 import java.io.*;
 
 public class FCFS {
@@ -5,7 +13,8 @@ public class FCFS {
     public static void main(String[] args) throws Exception {
 
         int processCount; // Number of processes
-        double averageWaitTime = 0; // Avg waiting time
+        double averageWaitingTime = 0; // Avg waiting time
+        double averageTurnaroundTime = 0; // Avg waiting time
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
@@ -13,14 +22,45 @@ public class FCFS {
         processCount = Integer.parseInt(br.readLine());
 
         int[] burstTime = new int[processCount]; // Burst time
-        int[] waitTime = new int[processCount]; // Waiting time
+        int[] waitingTime = new int[processCount]; // Waiting time
         int[] turnaroundTime = new int[processCount]; // Turnaround time
         int[] arrivalTime = new int[processCount]; // Arrival time
+
+        for (int i = 0; i < processCount; i++)
+        {
+            System.out.print("Enter Arrival Time of P[" +  (i + 1) + "]: ");
+            arrivalTime[i] = Integer.parseInt(br.readLine());
+        }
 
         for (int i = 0; i < processCount; i++)
         {
             System.out.print("Enter Process Burst Time of P[" +  (i + 1) + "]: ");
             burstTime[i] = Integer.parseInt(br.readLine());
         }
+
+        waitingTime[0] = 0; //waiting time for first process is 0
+        //calculating waiting time
+        for(int i = 1; i < processCount; i++)
+        {
+            waitingTime[i] = 0;
+            waitingTime[i] += burstTime[i - 1] + waitingTime[i - 1];
+            waitingTime[i] -= arrivalTime[i] - arrivalTime[i - 1];
+        }
+
+        System.out.println("Process \t Arrival Time \t Burst Time \t Waiting Time \t Turnaround Time");
+
+        //calculating turnaround time
+        for(int i = 0; i < processCount; i++)
+        {
+            turnaroundTime[i] = burstTime[i] + waitingTime[i];
+            averageWaitingTime += waitingTime[i];
+            averageTurnaroundTime += turnaroundTime[i];
+            System.out.println("P[" + (i + 1) + "]\t\t" + arrivalTime[i] + "\t\t" + burstTime[i] + "\t\t" + waitingTime[i] + "\t\t" + turnaroundTime[i]);
+        }
+
+        averageWaitingTime /= processCount;
+        averageTurnaroundTime /= processCount;
+        System.out.println("\n\nAverage Waiting Time: " + averageWaitingTime);
+        System.out.println("\nAverage Turnaround Time: " + averageTurnaroundTime);
    }
 }
